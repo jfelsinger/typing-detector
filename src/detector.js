e8fc03468eb5bba2db279b66109565e4e2724772
JFelsinger <JFFelsinger@gmail.com>
2015-02-21 23:48:09 -0500

    Added functionality for waiting before an action is detected

--- src/detector.js
+++ src/detector.js
@@ -8,12 +8,14 @@ var extend = require('../lib/extend'),
  *
  * Detects when an event starts and finishes
  */
-function Detector(name, timeout) {
+function Detector(name, timeout, preDetectionCount) {
     Emitter.call(this);
 
     this.isDetecting = false;
+    this.preDetectionCount = preDetectionCount || 0;
     this.timeout = timeout || 1000 * 5; // Time for when detection has timed out
 
+    this._count = 0;
     this._action = name || 'action';
     this._handle = null;
 }
@@ -27,21 +29,39 @@ extend(Detector, Emitter);
  */
 Detector.prototype.detect = function() {
     this.fire(this._action + '-detected');
+    this._count++;
 
-    if (this.isDetecting) {
-        // Clear the timer if we're still detecting an action
+    if (this._handle)
         clearTimeout(this._handle);
-    } else {
-        // Fire event if action just started again
-        this.fire(this._action + '-started');
-        this.isDetecting = true;
-    }
 
-    // Reset the timeout
-    this._handle = setTimeout(function() {
-        this.fire(this._action + '-stopped');
-        this.isTyping = false;
-    }.bind(this), this.timeout);
+    // If the count is higher than the detection count, we're safe to say
+    // that the event has started
+    if (this._count >= this.preDetectionCount) {
+
+        if (!this.isDetecting) {
+            // Fire event if action just started again
+            this.fire(this._action + '-started');
+            this.isDetecting = true;
+        }
+
+        // Reset the timeout
+        this._handle = setTimeout(function() {
+            this.fire(this._action + '-stopped');
+            this.isTyping = false;
+            this._count = 0;
+            this.fire('count-reset');
+        }.bind(this), this.timeout);
+
+    } else {
+
+        // Create a timeout to reset the counter if an action hasn't been
+        // detected enough times within in the timeout limit.
+        this._handle = setTimeout(function() {
+            this._count = 0;
+            this.fire('count-reset');
+        }.bind(this), this.timeout);
+
+    }
 
     return this;
 };
